cdd8ab76dd222a6e7953c560aabd218cc5a2aada
Robert Fox <rfox@osisoft.com>
2018-03-07 19:01:54 +1030

    added Ch1

---
+++ Ch1_Tutorial_Introduction/hello_world.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+// C does not require a return type, but C++ does
+// if C is given a function without a return type it assumes `int`
+// better to be explicit though
+int main()
+{
+printf("hello, world!\n");
+}
