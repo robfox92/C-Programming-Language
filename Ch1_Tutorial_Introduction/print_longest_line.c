1e559dcd3ac6485228ffef894c0a4e301bd09cc1
Robert Fox <fox.robert92@gmail.com>
2018-04-26 16:22:49 +0800

    change longest_line to use globals *shudder*

--- Ch1_Tutorial_Introduction/print_longest_line.c
+++ Ch1_Tutorial_Introduction/print_longest_line.c
@@ -2,22 +2,27 @@
 
 #define MAXLINE 1000 //maximum input line size
 
-int get_line( char line[], int maxline );
-void copy( char to[], char from[] );
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int get_line(void);
+void copy(void);
 
 //print longest input line
 
 int main()
 {
   int len = 0;           //current line length
-  int max = 0;           //maximum line length seen
-  char line[MAXLINE];    //current input line
-  char longest[MAXLINE]; //longest input line we know about
+  extern int max;
+  extern char longest[];
 
-  while ((len = get_line(line, MAXLINE)) > 0){ //get the line
+  max = 0
+
+  while ((len = get_line()) > 0){ //get the line
     if (len > max){                           //if the length is longer than the previous
       max = len;                              //longest, copy it to `longest` and save the length
-      copy(longest,line);
+      copy();
     }
   }
   if (max > 0){ //if there was a line, print it
@@ -27,29 +32,32 @@ int main()
 }
 
 // read a line in to s, return the length
-int get_line(char s[], int lim){
+int get_line(void){
   int c,i;
+  extern char line[];
 
   for (i = 0; 
-      i < lim-1 
+      i < MAXLINE-1 
       && (c = getchar()) != EOF 
       && c != '\n'; ++i){
-    s[i] = c;       //move through the input until you hit a EOF or \n
+    line[i] = c;       //move through the input until you hit a EOF or \n
   }
   if (c == '\n'){
-    s[i] = c;       //if the line is a single newline char
+    line[i] = c;       //if the line is a single newline char
     ++i;            //increment i
   }
-  s[i] = '\0';
+  line[i] = '\0';
   return i;
 }
 
 //copy `from` into `to`, assuming `to` is big enough
 //returns nothing (void)
 //relies on input being terminated by \0
-void copy(char to[], char from[]){
+void copy(void){
   int i = 0;
-  while ((to[i] = from[i]) != '\0'){
+  extern char line[], longest[];
+
+  while ((line[i] = longest[i]) != '\0'){
     ++i;
   }
 }
