4d9b0eb92ee0c837530570d91ee874a3b940c4ed
Robert Fox <fox.robert92@gmail.com>
2018-07-31 16:39:55 +0800

    added push and pop operations for stack.c

--- Ch4_Functions_Program_Structure/calc/stack.c
+++ Ch4_Functions_Program_Structure/calc/stack.c
@@ -6,10 +6,22 @@
 int sp = 0;
 double val[maxval];
 
-void push(double){
-  
+void push(double f){
+  if (sp < MAXVAL){
+    val[sp++] = f;
+  }
+  else{
+    printf("error: stack full, can't push %g\n", f);
+  }
 }
 
 double pop(void){
+  if (sp > 0){
+    return val[--sp];
+  }
+  else{
+    printf("error: stack empty\n");
+    return 0.0;
+  }
 
 }
