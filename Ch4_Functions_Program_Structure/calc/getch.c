a1a9c796858cd204350fe90b2a3e5b0afa05a7eb
Robert Fox <fox.robert92@gmail.com>
2018-07-31 16:56:18 +0800

    added getch and ungetch

--- Ch4_Functions_Program_Structure/calc/getch.c
+++ Ch4_Functions_Program_Structure/calc/getch.c
@@ -4,10 +4,18 @@
 char buf[BUFSIZE]
 int bufp = 0;
 
+// get a (possibly pushed back) character
 int getch(void){
-
+  return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
+// push a character back on input
 void ungetch(int){
+  if (bufp >= BUFSIZE){
+    printf("ungetch: too many characters");
+  }
+  else{
+    buf[bufp++] = c;
+  }
 
 }
