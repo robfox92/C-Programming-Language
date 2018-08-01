0cd16ba2aa8e0b8f0aa9eef8e4d2ea10e2f4d12d
Robert Fox <fox.robert92@gmail.com>
2018-08-01 16:59:57 +0800

    added getop()

--- Ch4_Functions_Program_Structure/calc/getop.c
+++ Ch4_Functions_Program_Structure/calc/getop.c
@@ -2,6 +2,26 @@
 #include <ctype.h>
 #include "calc.h"
 
-getop(){
+int getch(void);
+void ungetch(int);
 
+int getop(char s[]){
+  int i, c;
+
+  while((s[0] = c = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+  if (!isdigit(c) && c != '.')
+    return c;
+  if (isdigit(c))
+    while ((isdigit(s[++i]) = c = getch()))
+      ;
+  if (c == '.')
+    while ((isdigit(s[++i]) = c = getch()))
+      ;
+  s[i] = '\0';
+  if (c != EOF)
+    ungetch(c);
+  return NUMBER;
+  
 }
--- Ch4_Functions_Program_Structure/calc/main.c
+++ Ch4_Functions_Program_Structure/calc/main.c
@@ -3,6 +3,47 @@
 #include "calc.h"
 #define MAXOP 100
 
-main(){
+int getop(char []);
+void push(double);
+double pop(void);
 
+
+main(){
+  int type;
+  double op2;
+  char s[MAXOP];
+
+  while (( type = getop(s) ) != EOF){
+    switch (type){
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if (op2 != 0.0){
+          push(pop() / op2);
+        }
+        else{
+          printf("error: zero divisor")
+        }
+        break;
+      case '\n':
+        printf("\t%.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", s)
+        break;
+    }
+  }
+  return 0;
 }
